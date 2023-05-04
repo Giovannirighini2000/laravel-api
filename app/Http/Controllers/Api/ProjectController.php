a7ef198a23d22e174fbdc39694227b37e946e259
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-05-04 20:27:59 +0200

    parametric routes

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 use App\Models\Project;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,5 +13,20 @@ class ProjectController extends Controller
         return response()->json([
             'results'=>$results,
         ]);
+    }
+    public function show($slug){
+        $project = Project::where('slug', $slug)->first();
+        if($project){
+            return response()->json([
+            'success'=>true,
+            'project'=>$project,
+        ]);
+        } else {
+            return response()->json([
+                'success'=>false,
+                'project'=>'nessuno progetto trovato ',
+            ]);
+        }
+        
     }
 }
